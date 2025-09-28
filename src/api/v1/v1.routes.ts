2f8b4676692f3cd8ddfe9425cafde305a3de41cf
Juan Casadiego <esteban.casadiego@sinapsis.global>
2025-09-28 01:00:59 -0500

    cambio

--- src/api/v1/v1.routes.ts
+++ src/api/v1/v1.routes.ts
@@ -1,7 +1,7 @@
 import { routesAuth } from "@api/auth/infrastructure/routes/auth.routes";
 import { routesUsers } from "@api/users/infrastructure/routes/users.routes";
 import { routesRoles } from "@api/roles/infrastructure/routes/roles.routes";
-
+//s
 export {
     routesAuth as auth,
     routesUsers as users,
